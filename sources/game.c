3a13575a425951fe504ed229bb6e1493fd729357
Nairod36 <moulin.solal@gmail.com>
2022-12-04 18:28:47 +0100

    m

--- sources/game.c
+++ sources/game.c
@@ -478,15 +478,14 @@ void gestionGame(Donjon * d, ShootParams *shootParams, Boss * Boss, int stage, i
             GestionDoorsForMobRoom(d, stage, id, 1);
 		}
 
-        // if (c == 'm') {
+        if (c == 'm') {
 
+            Monster * monster = getMonsterById(arrayMonster, 1);
             
-        //     Monster * monster = getMonsterById(arrayMonster, 0);
-            
-        //     spawnMonster(d, monster, stage, id);
-        //     shootParams->monster = monster;
+            spawnMonster(d, monster, stage, id);
+            shootParams->monster = monster;
 
-        // }
+        }
 
 		if (c != 'e') {
  
@@ -780,31 +779,30 @@ void gestionGame(Donjon * d, ShootParams *shootParams, Boss * Boss, int stage, i
                 if (d->stages[stage].rooms[id].name == 'R'){
                     //Créer une liste de monstre aléatoire, d'une taille aléatoire
                     int randomNumberMonster = 2 + rand() % (5 - 2);
-                    // To fix
-                    Monster * newArrayMonster = malloc(sizeof(Monster) *randomNumberMonster +1);
+
+                    //Liste de monstre de la room
+                    d->stages[stage].rooms[id].newArrayMonster = malloc(sizeof(Monster) *randomNumberMonster +1);
 
                     for (int i = 0; i < randomNumberMonster; i++){
-                        //pour chaque iteration, on crée un monstre randomMonsterId
+                        //pour chaque iteration, on crée un id randomMonsterId
                         int randomMonsterId = 0 + rand() % (9 - 0);
                         //pour chaque iteration randomMonsterId doit être différent de ceux déjà stocké dans le tableau
                         for (int j = 0; j < i; j++){
-                            while (randomMonsterId == newArrayMonster[j].idMonster){
+                            while (randomMonsterId == d->stages[stage].rooms[id].newArrayMonster[j].idMonster){
                                 randomMonsterId = 0 + rand() % (9 - 0);
                             }
                         }
                         //ajout du monstre dans le tableau
                         Monster * monster = getMonsterById(arrayMonster,randomMonsterId);
-                        newArrayMonster[i] = *monster;
-                        //attributiopn de tableau de liste de monstre à la room
-                        d->stages[stage].rooms[id].newArrayMonster = newArrayMonster;    
+                        d->stages[stage].rooms[id].newArrayMonster[i] = *monster;
+                         
                     }
-                    //spawn des monstres de la liste la room dans la room
-                    for(int y = 0; y < randomNumberMonster; y++ ){
-                        Monster * monsterDisplay = getMonsterById(newArrayMonster,y);
-                        (void) monsterDisplay;
-                        // spawnMonster(d,monsterDisplay,stage,id);
+                    //spawn des monstres du tableau newArrayMonster dans la room
+                    // for(int y = 0; y < randomNumberMonster; y++ ){
+                       // Monster * monsterDisplay = getMonsterById( d->stages[stage].rooms[id].newArrayMonster,0);
+                      //  spawnMonster(d,monsterDisplay,stage,id);
 
-                    }
+                    // }
                 }
 
 
